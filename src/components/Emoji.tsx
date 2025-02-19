47246892f3c650ea7438e113a81e979cc26f675b
vat332 <oporowymakus21@gmail.com>
2025-02-19 23:29:23 +0100

    Add emoji

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,17 @@
+import { Text } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: string } = {
+    3: "😕",
+    4: "👍🏿",
+    5: "😵‍💫",
+  };
+  return <Text marginTop={1}>{emojiMap[rating]}</Text>;
+};
+
+export default Emoji;
