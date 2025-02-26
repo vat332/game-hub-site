fab45cc72d671ac71c660919c627b30fd28be7c6
vat332 <oporowymakus21@gmail.com>
2025-02-26 17:59:29 +0100

    Move entities and fix imports

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,19 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore from "../store";
-
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-  slug: number;
-  description_raw: string;
-}
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 const useGames = () => {
--- src/hooks/useGernes.ts
+++ src/hooks/useGernes.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import APIclient from "../services/api-client";
 import genres from "../data/genres";
 import ms from "ms";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIclient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
